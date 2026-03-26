86d1e97b344ccbcad29b184c044741532f8b3cec
lucamarchesi <lm.marchesi.luca@gmail.com>
2026-03-26 17:07:59 +0100

    Delete projects/python-tv-factor-model directory

--- projects/python-tv-factor-model/code/config.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-# === Percorsi ===
-ROOT = Path(__file__).resolve().parent
-DATA_DIR = ROOT / "data"
-FIG_DIR = ROOT / "Figures"
-
-# === File di input ===
-INPUT_FILE = DATA_DIR / "processed2.xlsx"
